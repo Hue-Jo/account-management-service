7211c7efe7d4b5d44b5d4577a62cda79f1882b13
HueJo <slack_water@naver.com>
2024-06-18 17:35:05 +0900

    mokito

--- AccountManagementService/src/test/java/com/example/accountmanagementservice/service/AccountServiceTest.java
+++ AccountManagementService/src/test/java/com/example/accountmanagementservice/service/AccountServiceTest.java
@@ -2,47 +2,66 @@ package com.example.accountmanagementservice.service;
 
 import com.example.accountmanagementservice.domain.Account;
 import com.example.accountmanagementservice.domain.AccountStatus;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.accountmanagementservice.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
 
-    @Autowired
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
     private AccountService accountService;
 
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void test123() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("6789")
+                        .build()));
+        //when
+        Account account = accountService.getAccount(4567L);
 
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
+        //then
+        assertEquals("6789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
-
 
     @Test
     @DisplayName("Test 1")
-    void testGetAccount1(){
+    void testGetAccount1() {
 
         accountService.createAccount();
         Account account = accountService.getAccount(1L);
 
         assertEquals("40000", account.getAccountNumber());
         assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-        }
+    }
 
     @Test
     @DisplayName("Test 2")
-    void testGetAccount2(){
+    void testGetAccount2() {
 
         accountService.createAccount();
         Account account = accountService.getAccount(2L);
 
         assertEquals("40000", account.getAccountNumber());
         assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-        }
+    }
 
 }
