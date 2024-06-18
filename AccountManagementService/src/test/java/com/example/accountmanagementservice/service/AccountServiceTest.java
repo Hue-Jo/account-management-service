5d281a4a8d95be0c08a123cc3871a0e1748d5ed5
HueJo <slack_water@naver.com>
2024-06-19 00:01:59 +0900

    accountServiceTest_createAccountSuccess

--- AccountManagementService/src/test/java/com/example/accountmanagementservice/service/AccountServiceTest.java
+++ AccountManagementService/src/test/java/com/example/accountmanagementservice/service/AccountServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.accountmanagementservice.service;
 
 import com.example.accountmanagementservice.domain.Account;
+import com.example.accountmanagementservice.domain.AccountUser;
+import com.example.accountmanagementservice.dto.AccountDto;
+import com.example.accountmanagementservice.repository.AccountUserRepository;
 import com.example.accountmanagementservice.type.AccountStatus;
 import com.example.accountmanagementservice.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -27,62 +30,41 @@ class AccountServiceTest {
     @Mock
     private AccountRepository accountRepository;
 
+    @Mock
+    private AccountUserRepository accountUserRepository;
+
     @InjectMocks
     private AccountService accountService;
 
     @Test
-    @DisplayName("계좌 조회 성공")
-    void testSuccess() {
+    void createAccountSuccess() {
         //given
-        given(accountRepository.findById(anyLong()))
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+
+        given(accountRepository.findFirstByOrderByIdDesc())
                 .willReturn(Optional.of(Account.builder()
-                        .accountStatus(AccountStatus.UNREGISTERED)
-                        .accountNumber("6789")
-                        .build()));
-        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
+                        .accountNumber("1000000012").build()));
+
+        given(accountRepository.save(any()))
+                .willReturn(Account.builder()
+                                .accountUser(user)
+                        .accountNumber("1000000015").build());
+
+        ArgumentCaptor<Account> captor = ArgumentCaptor.forClass(Account.class);
+
         //when
-        Account account = accountService.getAccount(4567L);
+        AccountDto accountDto = accountService.createAccount(1L, 1000L);
 
         //then
-        verify(accountRepository, times(1)).findById(captor.capture());
-        verify(accountRepository, times(0)).save(any());
-        assertEquals(4567L, captor.getValue());
-        assertEquals("6789", account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+        verify(accountRepository, times(1)).save(captor.capture());
+        assertEquals(12, accountDto.getUserId());
+        assertEquals("1000000013", captor.getValue().getAccountNumber());
     }
 
-    @Test
-    @DisplayName("계좌 조회 실패 - 음수로 조회")
-    void testFailedToSearch() {
-        //given
-        //when
-        RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> accountService.getAccount(-10L));
-
-        //then
-        assertEquals("Minus", exception.getMessage());
-    }
-
-//    @Test
-//    @DisplayName("Test 1")
-//    void testGetAccount1() {
-//
-//        accountService.createAccount();
-//        Account account = accountService.getAccount(1L);
-//
-//        assertEquals("40000", account.getAccountNumber());
-//        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-//    }
-//
-//    @Test
-//    @DisplayName("Test 2")
-//    void testGetAccount2() {
-//
-//        accountService.createAccount();
-//        Account account = accountService.getAccount(2L);
-//
-//        assertEquals("40000", account.getAccountNumber());
-//        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-//    }
 
 }
